dbd7ffd8004d1cc48d14464bff178e95312d4c40
Yashwant Jodha <ysjodha1@gmail.com>
2023-03-30 11:03:38 +0530

    update main

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import mysql.connector
 from mysql.connector import Error
 
 import sys
+import requests
 
 
 ##########
 # CONFIG #
 ##########
 db_client = MongoClient()
-db = db_client["email_automation"]
+db_name = "email_automation"
+db = db_client[db_name]
 tags_collection = db["TAGS"]
 
 # schema_type
@@ -40,7 +42,7 @@ print()
 # tag
 while True:
     tag = input("Enter TAG(should be unique): ").lower()
-    if tags_collection.count_documents({"tag": tag}):
+    if collection.count_documents({"TAG": tag}):
         print(
             f"** Warning: '{tag}' tag is already in use, it should be unique for each list.."
         )
@@ -57,7 +59,8 @@ try:
     email_read_cursor = email_db_connection.cursor(buffered=True)
 except Error as e:
     print("** Error while connecting to MySQL", e)
-
+    sys.exit()
+    
 
 ######################
 # EMAIL VERIFICATION #
@@ -87,10 +90,52 @@ for idx, row in df.iterrows():
 email_db_connection.commit()
 print("** All emails inserted to email database")
 
+print()
+
 # run the email verification on new data
 # TODO: use pywinauto to automate the gui process
-print("** VERIFY EMAILS NOW: Open Email verifier -> data source -> start (verify new)")
-
+print("** VERIFY EMAILS NOW: Open 'Email verifier' -> data source -> start (verify new)")
 confirmation = ""
 while confirmation != "y":
-    confirmation = input("verification complete ? press y and hit enter: ")
+    confirmation = input("press 'y' and hit enter when verification is complete: ")
+
+# reverse lookup emails in the database
+query = "SELECT * FROM emails WHERE email = %s"
+data = []
+for idx, row in df.iterrows():
+    doc = {}
+    for x in df.columns:
+        if x not in email_fields:
+            doc[x] = row[x]
+
+    doc['emails'] = []
+    
+    for email_field in email_fields:
+        if row[email_field]:
+            email_obj = {}
+            email = row[email_field]
+            email_read_cursor.execute(query, (email,))
+            record = email_read_cursor.fetchone()
+            if not record:
+                raise Error("Email not found...")
+            
+            email_obj = {
+                'Email': record[1],
+                'StatusCode': record[2],
+                'Result': record[3],
+                'Description': record[4],
+                'SMTPServer': record[5],
+                'SMTPServerReply': record[6],
+                'Date': record[7],
+                'Log': record[8],
+            }
+            doc['emails'].append(email_obj)
+
+    doc['ENRICHED'] = False
+    doc['SCHEMA_TYPE'] = schema_type
+    doc['TAG'] = tag
+    data.append(doc)
+
+collection.insert_many(data)
+print(f'** data inserted to the {db_name}/{company_name}')
+# TODO: create TAGS collection with tags and other details
